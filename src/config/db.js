76a5b1d43e4839849570d0c8947e95dd03bcae4c
Ajaykvishwakarma <ajaykv054@gmail.com>
2022-07-23 14:18:01 +0530

    O-Nine

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,3 @@
-// const mongoose = require("mongoose");
-// require("dotenv").config();
-// module.exports = () => mongoose.connect(process.env.MONGODB_URL);
-
-
 const mongoose = require("mongoose");
-
-const connect = () => {
-    return mongoose.connect(
-        "mongodb+srv://ajaymessanger:ajaymessanger@cluster0.bjjpj.mongodb.net/?retryWrites=true&w=majority"
-        // "mongodb://localhost:27017/messanger"
-    )
-}
-module.exports = connect;
+require("dotenv").config();
+module.exports = () => mongoose.connect(process.env.MONGODB_URL);
